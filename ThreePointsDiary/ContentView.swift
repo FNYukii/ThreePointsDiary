ab8474345260f9a4089bb2606c82969b1e5b9b2c
Yu357 <forestgreen14689@gmail.com>
2021-09-10 00:00:42 +0900

    日記一覧リストを仮配置

--- ThreePointsDiary/ContentView.swift
+++ ThreePointsDiary/ContentView.swift
@@ -11,9 +11,28 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             
-            Text("ここに日記一覧を表示する予定")
-                .padding()
+            Form {
+                Section(header: Text("2021-09-09 Thu")) {
+                    Text("Webデザインの練習")
+                    Text("SwiftUIの練習")
+                    Text("買い物した")
+                }
+                
+                Section(header: Text("2020-09-08 Wed")) {
+                    Text("ゲーム楽しかった")
+                    Text("料理した")
+                    Text("いっぱい寝た")
+                }
+                
+                Section(header: Text("2020-09-07 Tue")) {
+                    Text("風邪が治った")
+                    Text("買い物に行った")
+                    Text("洗濯した")
+                }
+            }
             
+                
+                
             .navigationBarTitle("3 Points Diary", displayMode: .inline)
             .navigationBarItems(trailing:
                 Button("White"){
