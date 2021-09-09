c28276083a88e0c796c8da50d3149ae350e66253
Yu357 <forestgreen14689@gmail.com>
2021-09-10 00:37:08 +0900

    日記編集画面にFormを配置した

--- ThreePointsDiary/EditView.swift
+++ ThreePointsDiary/EditView.swift
@@ -12,23 +12,36 @@ struct EditView: View {
     //この画面の環境変数
     @Environment(\.presentationMode) var presentation
     
+    //その日のできごと
+    @State var point01 = ""
+    @State var point02 = ""
+    @State var point03 = ""
+    
     var body: some View {
         NavigationView {
             
-            Text("このSheetで日記を編集する")
+            //日記入力フォーム
+            Form {
+                Section(header: Text("2021-09-09 The")){
+                    TextField("できごとその1", text: $point01)
+                    TextField("できごとその2", text: $point02)
+                    TextField("できごとその3", text: $point03)
+                }
+            }
             
-                .navigationBarTitle("今日の日記", displayMode: .inline)
-                .navigationBarItems(
-                    leading:
-                    Button("キャンセル"){
-                        presentation.wrappedValue.dismiss()
-                    },
-                    trailing:
-                    Button("完了"){
-                        saveDiary()
-                        presentation.wrappedValue.dismiss()
-                    }
-                )
+            //ナビゲーションバーの設定
+            .navigationBarTitle("今日の日記", displayMode: .inline)
+            .navigationBarItems(
+                leading:
+                Button("キャンセル"){
+                    presentation.wrappedValue.dismiss()
+                },
+                trailing:
+                Button("完了"){
+                    saveDiary()
+                    presentation.wrappedValue.dismiss()
+                }
+            )
 
         }
     }
