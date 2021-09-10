42b66ec53f795cc33906ab71dc9a81fdc556e538
Yu357 <forestgreen14689@gmail.com>
2021-09-10 14:08:27 +0900

    ホーム画面でも年月日と曜日がいい感じに表示されるようになった

--- ThreePointsDiary/ContentView.swift
+++ ThreePointsDiary/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //Date型変数から詳細な情報を取り出すためのCalendarクラス
+    let calendar = Calendar(identifier: .gregorian)
+    
     //日記編集シートのオンオフ
     @State var isShowSheet = false
     
@@ -21,7 +24,7 @@ struct ContentView: View {
             //日記一覧
             Form {
                 ForEach(diarys.freeze()) { diary in
-                    Section(header: Text("\(diary.createdDate)")) {
+                    Section(header: Text("\(ymdText(createdDate: diary.createdDate)) \(weekdayText(createdDate: diary.createdDate))")) {
                         Text(diary.content01)
                         Text(diary.content02)
                         Text(diary.content03)
@@ -44,6 +47,25 @@ struct ContentView: View {
                 
         }
     }
+    
+    //Date型変数を年月日のみの文字列に変換する
+    func ymdText(createdDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        dateFormatter.dateStyle = .medium
+        dateFormatter.dateFormat = "yyyy年 MM月 dd日"
+        return dateFormatter.string(from: Date())
+    }
+    
+    //Date型変数を曜日のみの文字列に変換する
+    func weekdayText(createdDate: Date) -> String {
+        let weekdayNumber = calendar.component(.weekday, from: Date())
+        let weekdaySymbolIndex: Int = weekdayNumber - 1
+        let formatter: DateFormatter = DateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "ja") as Locale
+        return formatter.shortWeekdaySymbols[weekdaySymbolIndex]
+    }
+    
 }
 
 
