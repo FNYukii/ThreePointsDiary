23e19df04b247e0f082f835e02e52cc9dd735309
Yu357 <forestgreen14689@gmail.com>
2021-09-11 15:39:19 +0900

    コードを一旦掃除した

--- ThreePointsDiary/SecondView.swift
+++ ThreePointsDiary/SecondView.swift
@@ -11,22 +11,8 @@ import RealmSwift
 struct SecondView: View{
     
     //カレンダー上で選択された日付
-    @State var selectedDate = Date() {
-        willSet(newValue) {
-            print(newValue)
-            searchDiary()
-        }
-    }
-    
-    //検索条件に一致する日記
-    @State var diaries = Diary.all()
-    
-    //日記編集シートのオンオフ
-    @State var isShowSheet = false
-    
-    //編集対象の日記
-    @State var selectedDiaryId = 0
-    
+    @State var selectedDate = Date()
+        
     var body: some View {
         
         Form {
@@ -37,73 +23,18 @@ struct SecondView: View{
                         searchDiary()
                     })
             }
-            Text("\(selectedDate)")
-            ForEach(diaries.freeze()) { diary in
-                Section(header: Text("\(ymdText(inputDate: diary.createdDate)) \(weekdayText(inputDate: diary.createdDate))")) {
-                    Button("\(diary.content01)"){editDiary(diaryId: diary.id)}
-                        .foregroundColor(.primary)
-                    Button("\(diary.content02)"){editDiary(diaryId: diary.id)}
-                        .foregroundColor(.primary)
-                    Button("\(diary.content03)"){editDiary(diaryId: diary.id)}
-                        .foregroundColor(.primary)
-                }
+            Section(header: Text("2021年 9月 7日 火")) {
+                Text("Homework completed")
+                Text("Shopping done")
+                Text("slept a lot")
             }
-                
         }
         
     }
-    
-    //タップした日記を編集
-    func editDiary(diaryId: Int) {
-        selectedDiaryId = diaryId
-        isShowSheet.toggle()
-    }
-    
-    //Date型変数を年月日のみの文字列に変換する
-    func ymdText(inputDate: Date) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ja_JP")
-        dateFormatter.dateStyle = .medium
-        dateFormatter.dateFormat = "yyyy年 M月 d日"
-        return dateFormatter.string(from: inputDate)
-    }
-    
-    //Date型変数を曜日のみの文字列に変換する
-    func weekdayText(inputDate: Date) -> String {
-        let calendar = Calendar(identifier: .gregorian)
-        let weekdayNumber = calendar.component(.weekday, from: inputDate)
-        let weekdaySymbolIndex: Int = weekdayNumber - 1
-        let formatter: DateFormatter = DateFormatter()
-        formatter.locale = NSLocale(localeIdentifier: "ja") as Locale
-        return formatter.shortWeekdaySymbols[weekdaySymbolIndex]
-    }
-    
+        
     //選択された日に作成された日記を検索する
     func searchDiary() {
-                
-        //開始日時
-        let calendar = Calendar(identifier: .gregorian)
-        let startDate = calendar.startOfDay(for: selectedDate)
-        
-        //終了日時
-        var compornets = calendar
-            .dateComponents([.year, .month, .day,
-                             .hour, .minute, .second,
-                             .nanosecond],
-                             from: selectedDate)
-        compornets.hour = 23
-        compornets.minute = 59
-        compornets.second = 59
-        let endDate = calendar.date(from: compornets)
-        
-        print("start: \(startDate)")
-        print("end: \(endDate)")
-        print("selectedDate: \(selectedDate)")
-        print("Date(): \(Date())")
-        print("---------")
-
-        let realm = try! Realm()
-//        diaries = realm.objects(Diary.self).filter("createdDate >= \(startDate) && createdDate < \(endDate)")
+        //日記を検索する
     }
     
 }
